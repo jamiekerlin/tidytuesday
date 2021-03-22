4ff22b8ee2a771acc20bf81a52406e745b952acc
Jamie Kerlin <jamiekerlin@gmail.com>
2021-03-22 14:24:00 -0700

    tidytuesday update

---
+++ Scripts/2021_03_16.R
@@ -0,0 +1,31 @@
+### Tidy Tuesday 2021-03-16 ####################################
+# Gaming
+# Created by Jamie Kerlin
+# Created on 2021-03-22
+###############################################################
+
+### Load libraries ############################################
+library(tidyverse)
+library(tidytuesdayR)
+library(here)
+library(gganimate)
+
+### Load data #################################################
+tuesdata <- tidytuesdayR::tt_load('2021-03-16')
+games <- tuesdata$games
+
+### Clean data ################################################
+games$month <- factor(games$month, levels=c("December", "November",
+                                            "October", "September", "August",
+                                            "July", "June", "May",
+                                            "April", "March", 
+                                            "February", "January"))
+
+### Create graph ##############################################
+ggplot(data = games,
+       mapping = aes(x = month, 
+                     y = gain)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  transition_states(
+    year)
