6872b98ac4ef0e552ea747852118953566b03eb0
Jamie Kerlin <jamiekerlin@gmail.com>
2021-03-26 19:26:07 -0700

    2021_03_16 tidy tuesday complete

--- Scripts/2021_03_16.R
+++ Scripts/2021_03_16.R
@@ -9,45 +9,48 @@ library(tidyverse)
 library(tidytuesdayR)
 library(here)
 library(gganimate)
-library(ggstream)
+library(PNWColors)
 
 
 ### Load data #################################################
-tuesdata <- tidytuesdayR::tt_load('2021-03-16')
-games <- tuesdata$games
+tuesdata <- tidytuesdayR::tt_load #load data from tidytuesday
+games <- tuesdata$games #save data set to environment
 
 ### Clean data ################################################
-games$month <- factor(games$month, levels=c("December", "November",
-                                            "October", "September", "August",
-                                            "July", "June", "May",
-                                            "April", "March", 
-                                            "February", "January"))
+games <- games %>%
+  filter(year == "2021") %>% #filter by 2021
+  mutate(avg_peak_percent = (avg/peak) * 100) %>% #recalculate so it is numeric
+  select(!c(year, month, avg_peak_perc)) #deselect columns not needed
+
+games1 <- games %>%
+  filter(avg > 30000.0) %>% #filter for highest average simultaneous players
+  pivot_longer(2:5, #pivot longer by all popularity variables
+               names_to = "variables",
+               values_to = "values")
+  
+games2 <- games3 %>% 
+  group_by(variables) %>% #group by variables
+  mutate(std_values = (values - mean(values))/sd(values)) #standardized values for heatmap
+
 
 ### Create graph ##############################################
-ggplot(data = games,
-       mapping = aes(x = month, 
-                     y = gain)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  transition_states(
-    year)
+pnw1 <- rev(pnw_palette(6)) #create color palette object
 
-
-ggplot(data = games,
-       mapping = aes(x = year, 
-                     y = peak,
-                     fill = month)) + 
-  geom_stream()
-
-
-ggplot(games, aes(year, gain, fill = month)) +
-  geom_stream()
-
-
-
-
-
-
-
-
+ggplot(games2, mapping = aes(x = variables, #x axis is variables
+                             y = gamename, #y axis is videogame names
+                             fill = std_values)) + #filling by standardized values
+  geom_tile() + #geom tile for ggplot heatmap
+  scale_fill_gradientn(colors = pnw1) + #add previously saved palette to plot
+  labs(x = "Variables", #label x, y, legend, title, and caption
+       y = "Name of videogame",
+       fill = "Standardized Values",
+       title = "2021 Popularity of Videogames on Steam",
+       caption = "Data from SteamCharts | Code at github.com/jamiekerlin") +
+  theme_light() + #set theme
+  scale_y_discrete(limits=rev) + #reverse y axis order
+  scale_x_discrete(labels = c("Average", "Percent Average Peak", "Gain", "Peak")) +
+#rename variables
+  ggsave(here("Output",
+              "2021_03_16",
+              "2021_03_16_heatmap.png"))
 
